35aa3b113fc026b2321afc0a1c87c3bf8a6edbfe
Евгений Зуев <carnage02@yandex.ru>
2022-12-18 17:18:05 +0300

    Seventh version

--- yatube/posts/utils.py
+++ yatube/posts/utils.py
@@ -1,7 +1,7 @@
 from django.core.paginator import Paginator
 
 
-def Page(request, post_list, limit):
-    paginator = Paginator(post_list, limit)
-    page_number = request.GET.get('page')
+def Page(request_, post_list, limit_):
+    paginator = Paginator(post_list, limit_)
+    page_number = request_.GET.get('page')
     return paginator.get_page(page_number)
